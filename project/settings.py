0a7398f4023f985cb131321fa1068fea13c8f6ee
nonzoo <nonsoosadebe202@gmail.com>
2023-07-07 16:58:08 +0100

    updated6

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1" , "naijaket.com", 'www.naijaket.com','137.184.158.12']
 
